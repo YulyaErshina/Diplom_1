0e14e86258e9e1f6862ad7f87f4769eeb533038c
Julia Ershina <julia.ershina@dataart.com>
2022-06-07 11:43:28 +0300

    add Diplom_1

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    String name = "black bun";
+    float price = 100;
+
+    @Test
+    public void getBunNameTest() {
+        Bun bun = new Bun(name, price);
+        String expected = "black bun";
+        assertEquals(expected, bun.getName());
+    }
+
+    @Test
+    public void getBunPriceTest() {
+        Bun bun = new Bun(name, price);
+        float expected = 100;
+        assertEquals(expected, bun.getPrice(), 0);
+    }
+
+}
---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTest {
+
+    IngredientType type = IngredientType.SAUCE;
+    String name = "cutlet";
+    float price = 100;
+
+    @Test
+    public void getIngredientPriceTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        float expected = 100;
+        assertEquals(expected, ingredient.getPrice(), 0);
+    }
+
+    @Test
+    public void getIngredientNameTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        String expected = "cutlet";
+        assertEquals(expected, ingredient.getName());
+    }
+
+    @Test
+    public void getIngredientTypeTest() {
+        Ingredient ingredientSauce = new Ingredient(type, name, price);
+        assertEquals(IngredientType.SAUCE, ingredientSauce.getType());
+    }
+}
